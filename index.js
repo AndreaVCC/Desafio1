fdad62a0d80cb3f827e9c657d5334f024fdce3c2
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-05-06 17:58:53 -0400

    Guardando ultimos cambio, agregndo imagen fondo

--- index.js
+++ index.js
@@ -1,76 +1,76 @@
- 
  firebase.initializeApp({
-      apiKey: "AIzaSyAMqZJ62i7jgOj8GL51lC5M4sK4bpxYdcw",
-      authDomain: "desafio1comentarios.firebaseapp.com",
-      databaseURL: "https://desafio1comentarios.firebaseio.com",
-      projectId: "desafio1comentarios",
-      storageBucket: "desafio1comentarios.appspot.com",
-      messagingSenderId: "441785957167"
-  })
-  let db = firebase.firestore();
+     apiKey: "AIzaSyAMqZJ62i7jgOj8GL51lC5M4sK4bpxYdcw",
+     authDomain: "desafio1comentarios.firebaseapp.com",
+     databaseURL: "https://desafio1comentarios.firebaseio.com",
+     projectId: "desafio1comentarios",
+     storageBucket: "desafio1comentarios.appspot.com",
+     messagingSenderId: "441785957167"
+ })
+ let db = firebase.firestore();
 
 
 
-  //GUARDAR VALOR INPUT EN FIREBASE
-  function uploadComment() {
-      let comentario = document.getElementById('comment').value;
+ //GUARDAR VALOR INPUT EN FIREBASE
+ function uploadComment() {
 
-      
-      db.collection('Comentarios').add({
-              comentario: comentario
-          })
-          .then(function (docRef) {
-              console.log('Comentario subido');
-              document.getElementById('comentario').value = '';
+    
+     let comentario = document.getElementById('comment').value;
 
-          })
-          .catch(function (error) {
-              console.log('Error');
-          });
-  }
+     if (comentario.length >= 25) {
+        alert("Ingrese menos de 25 carácteres");
+    } else {
+        db.collection('Comentarios').add({
+            comentario: comentario
+        })       
+        .then(function (docRef) {
+            console.log('Comentario subido');
+            // document.getElementById('comentario').value = '';
+        })
+        .catch(function (error) {
+            console.log('Error');
+        });
+    }
+    
+ }
 
-  //MOSTRAR COLECCION DE FIREBASE
+ //MOSTRAR COLECCION DE FIREBASE
 
-  let contenido = document.getElementById('contenido');
+ let contenido = document.getElementById('contenido');
 
-db.collection('Comentarios').onSnapshot((querySnapshot) => {
-    contenido.innerHTML ='';
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        contenido.innerHTML += `
-        <div class='container row'>
-            <div class=' col s12'>
+ db.collection('Comentarios').onSnapshot((querySnapshot) => {
+     contenido.innerHTML = '';
+     querySnapshot.forEach((doc) => {
+         contenido.innerHTML += `
+        
                 <div class='card'>
-                    <div class ='row'>
-                        <div class='col s10'>
-                            <p>Comentario:</p>
+                        <div class='card-title'>
+                            <div class='col m11 s11'> 
+                                <span class="flow-text">Recordar:</span>
+                            </div>
+                            <div class='col m1 s1'>
+                                <button class="btn pink darken-1" type="submit" onclick="eliminated('${doc.id}')"><i class="material-icons medium">
+                                delete_forever
+                                </i></button>
+                            </div>
                         </div>
-                        <div class='col s2'>
-                            <button class="btn red darken-1" type="submit" id="btnsave"onclick="eliminated('${doc.id}')">X</button>
+                                   
+                        <div class='card-content'>
+                            <p>${doc.data().comentario}</p>
                         </div>
-                    </div>
-                    <div class='row'>
-                        <div class='col s12'>
-                            ${doc.data().comentario}
-                        </div>
-                    </div>
                 </div>
-            </div>            
-        </div>
+         
        
         `
-    });
-});
+     });
+ });
 
-//BORRAR
+ //BORRAR
 
-function eliminated(a){
-    db.collection('Comentarios').doc(a).delete()
-    .then(function() {
-        console.log ('Borrado')
-    }).catch(function(error) {
-        console.log('Error al eliminar');
-    })
-}
-
-
+ function eliminated(a) {
+     db.collection('Comentarios').doc(a).delete()
+         .then(function () {
+             console.log('Borrado')
+         }).catch(function (error) {
+             console.log('Error al eliminar');
+         })
+ }
